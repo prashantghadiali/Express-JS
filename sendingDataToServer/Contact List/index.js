7d441d6d7994a680c8394f4574f579d9a0cc41d2
prashantghadiali <er.prashantghadiali@gmail.com>
2023-03-19 11:10:57 +0530

    index changed

--- sendingDataToServer/Contact List/index.js
+++ sendingDataToServer/Contact List/index.js
@@ -6,6 +6,24 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded());
+
+// middleware1
+app.use(function(req, res, next){
+   
+
+    req.myName = "Arpan"
+    // console.log('middleware 1 called');
+    next();
+});
+
+// middleware2
+app.use(function(req, res, next){
+    
+    console.log('My name called from MW2', req.myName);
+    // console.log('middleware 2 called');
+    next();
+});
 
 
 var contactList = [
@@ -31,13 +49,17 @@ app.get('/practice', function(req, res){
 
 
 app.get('/', function(req, res){
+    console.log('from the get route controller', req.myName);
     return res.render('home',{
         title: "Contact List",
         contact_list: contactList
     });
 })
 app.post('/create-contact', function(req, res){
-    return res.redirect('/practice');
+ 
+    contactList.push(req.body);
+    return res.redirect('/');
+
 });
 
 app.listen(port, function(err){
